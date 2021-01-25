07eada4a2b7a5bbb04a988a232bd3af99a4445ec
Joshua Miller <joshsmiller1022@gmail.com>
2021-01-25 13:34:47 -0700

    initial commit

--- classes/practice-classes.js
+++ classes/practice-classes.js
@@ -15,7 +15,19 @@
 */
 
 //CODE HERE
+class Character {
+  constructor (name, type) {
+    this.name = name,
+    this.type = type
+  }
+  getInfo() {
+    return `This is a ${this.type} character named ${this.name}.`
+  }
+}
+const luigi = new Character('Luigi', 'Italian')
+luigi.getInfo()
 
+// export default Character;
 //////////////////PROBLEM 2////////////////////
 
 /*
@@ -33,6 +45,18 @@
 */
 
 //CODE HERE
+class NPC extends Character {
+  constructor (name, type, location, phrase) {
+    super(name, type);
+    this.location = location,
+    this.phrase = phrase
+  }
+  
+  dialogue () {
+    return `${this.name}: ${this.phrase}`
+  }
+}
+
 
 /*
     Create an NPC named Ralph who is a human located in Niceland. His phrase should be `I'm gonna wreck it!`. 
@@ -40,6 +64,8 @@
 */
 
 //CODE HERE
+const ralph = new NPC('Ralph', 'human', 'Niceland', "I'm gonna wreck it!")
+
 
 /*
     Next you're going to create three variables to store information about Ralph.
@@ -49,7 +75,9 @@
 */
 
 //CODE HERE
-
+const ralphsInfo = ralph.getInfo()
+const ralphsDialogue = ralph.dialogue()
+const ralphsLocation = ralph.location
 //////////////////PROBLEM 3////////////////////
 
 /*
@@ -74,7 +102,20 @@
 */
 
 //CODE HERE
+class Player extends Character{
+  constructor(name, type, healthLevel, attackLevel){
+    super(name, type);
+      this.healthLevel = healthLevel,
+      this.attackLevel = attackLevel
+  }
+  defend(amount){
+    
+    // if (this.healthLevel > 0){
+    //   return 
+    // }
 
+  }
+}
 /*
     Next, we'll create two Players.
     Store the first in a variable called aang, his name should be 'Aang' 
@@ -84,7 +125,8 @@
 */
 
 //CODE HERE
-
+const aang = new Player('Aang', 'airbender', 100, 100)
+const ozai = new Player('Ozai', 'firebender', 100, 0)
 /*
     Let's see how a fight between these two would go. 
     Create a variable called 'battle' whose value is Ozai's 
@@ -93,7 +135,7 @@
 */
 
 //CODE HERE
-
+const battle = ozai.defend(aang.attackLevel)
 //////////////////PROBLEM 4////////////////////
 
 /*
--- closures/practice-closures.js
+++ closures/practice-closures.js
@@ -9,12 +9,18 @@
   */
   
   //CODE HERE
+  function myFunc(){
+    let myStr = 'super secret string'
 
+    return function getSecretString(){
+      return myStr
+    }
+  }
   
   //Now create a variable called secretString. Its value should be the invocation of myFunc.
 
   //CODE HERE
-  
+  const secretString = myFunc()
   
   ////////////PROBLEM 2////////////
 
@@ -29,22 +35,36 @@
   */
   
   //CODE HERE
+  function lightSwitch(){
+    let isTheLightOn = false
 
+    function flipTheSwitch(){
+      // isTheLightOn = true
+      if(isTheLightOn === false){
+        isTheLightOn = true
+        return `The light is on`
+      } else {
+        isTheLightOn = false
+        return `The light is off`
+      }
+    }
+    return flipTheSwitch()
+  }
   
   //Create a variable called kitchenSwitch whose value is the invocation of lightSwitch.
   
   //CODE HERE
-
+  let kitchenSwitch = lightSwitch()
   
   //Invoke kitchenSwitch.
 
   //CODE HERE
-  
+  kitchenSwitch()
   
   //Create a variable called bathroomSwitch whose value is the invocation of lightSwitch. 
 
   //CODE HERE
-  
+  let bathroomSwitch = lightSwitch()
   
   //Invoke bathroomSwitch twice.
   
@@ -58,13 +78,30 @@
     Use the module pattern to create a plant height tracker, name your function 'plantTracker'
     Set up two variables inside plantTracker, 'plant' which should be set to 'fern' and 'height' which should be set to 12
     Return 3 functions using the module pattern
-      - 'readInfo' should return a string of 'This is a PLANT plant that is HEIGHT inches tall.', where PLANT is the value of the plant variable and HEIGHT is the value of the height variable.
+      - 'readInfo' should return a string of 'This is a PLANT plant that is HEIGHT inches tall.', where PLANT is the value of the plant variable and 
+      HEIGHT is the value of the height variable.
       - 'waterPlant' should add 1 to the height and return the new height
       - 'prunePlant' should subtract 1 from the height and return the new height
   */
 
   //CODE HERE
-
+  function plantTracker() {
+    let plant = 'fern'
+    let height = 12
+  
+    return {
+      readInfo: () => {
+       return `This is a ${plant} plant that is ${height} inches tall.`
+      },
+      waterPlant: () => {
+        return height += 1
+      },
+      prunePlant: () => {
+        return height -= 1
+      }
+    }
+  }
+  
 
   ////////////PROBLEM 4////////////
 
